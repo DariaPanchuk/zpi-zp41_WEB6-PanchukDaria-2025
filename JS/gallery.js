9800304ff3430a521353e10e67ee01ef60cbf73f
DariaPanchuk <105421559+DariaPanchuk@users.noreply.github.com>
2025-04-22 19:59:16 +0300

    bugfix

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,65 +1,65 @@
 const images = [
 {
     preview:
-    '/img/1.png',
+    'img/1.png',
     original:
-    '/img/modal/m-1.webp',
+    'img/modal/m-1.webp',
     description: 'Планування',
 },
 {
     preview:
-    '/img/2.png',
+    'img/2.png',
     original:
-    '/img/modal/m-2.jpg',
+    'img/modal/m-2.jpg',
     description: 'Ідея',
 },
 {
     preview:
-    '/img/3.png',
+    'img/3.png',
     original:
-    '/img/modal/m-3.jpg',
+    'img/modal/m-3.jpg',
     description: 'Телефон',
 },
 {
     preview:
-    '/img/4.png',
+    'img/4.png',
     original:
-    '/img/modal/m-4.webp',
+    'img/modal/m-4.webp',
     description: 'Нарада',
 },
 {
     preview:
-    '/img/5.png',
+    'img/5.png',
     original:
-    '/img/modal/m-5.avif',
+    'img/modal/m-5.avif',
     description: 'Офіс',
 },
 {
     preview:
-    '/img/6.png',
+    'img/6.png',
     original:
-    '/img/modal/m-6.jpg',
+    'img/modal/m-6.jpg',
     description: 'Робота',
 },
 {
     preview:
-    '/img/7.png',
+    'img/7.png',
     original:
-    '/img/modal/m-7.jpg',
+    'img/modal/m-7.jpg',
     description: 'Кава',
 },
 {
     preview:
-    '/img/8.png',
+    'img/8.png',
     original:
-    '/img/modal/m-8.webp',
+    'img/modal/m-8.webp',
     description: 'Ноутбук',
 },
 {
     preview:
-    '/img/9.png',
+    'img/9.png',
     original:
-    '/img/modal/m-9.jpg',
+    'img/modal/m-9.jpg',
     description: 'Презентація',
 },
 ];
